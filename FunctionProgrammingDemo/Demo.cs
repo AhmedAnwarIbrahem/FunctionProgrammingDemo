ce0350ad3db0e0a27962b7e5cde74fc64f12609f
A.Anwar <A.Anwar@DESKTOP-U8B1Q31>
2022-01-04 17:02:19 +0200

    changes

--- FunctionProgrammingDemo/Demo.cs
+++ FunctionProgrammingDemo/Demo.cs
@@ -19,9 +19,9 @@ namespace FunctionProgrammingDemo
         }
         private Order GetOrderwithDiscount(Order order, List<(Func<Order, bool> QualifyingCondition, Func<Order, double> GetDiscount)> Rules)
         {
-            var discount = Rules.Where((a) => a.QualifyingCondition(order)).Select((b) => b.GetDiscount(order)).OrderBy((x) => x).Take(3);
-            order.Discount = discount.Any() ? discount.Average() : 0;
-            //order.Discount = discount.LastOrDefault();
+            var discounts = Rules.Where(a => a.QualifyingCondition(order)).Select(b => b.GetDiscount(order)).OrderBy(x => x).Take(3);
+            order.Discount = discounts.Any() ? discounts.Average() : 0;
+            //order.Discount = discounts.LastOrDefault();
             return order;
         }
         private List<(Func<Order, bool> QualifyingCondition, Func<Order, double> GetDiscount)> GetDiscountRules()
@@ -31,45 +31,50 @@ namespace FunctionProgrammingDemo
               (isAQualified, A),
               (isBQualified, B),
               (isCQualified, C),
-              (isDQualified, D)
+              (isDQualified, D),
+              //(isAQualified, C)
             };
             return DiscountRules;
         }
 
 
 
-        //if Quantity more than 10 get 50% Discount
+        //if Quantity more than 10 
         public bool isAQualified(Order r)
         {
             return r.Quantity > 10;
         }
+        //get 50% Discount
         public double A(Order r)
         {
             return r.Price * 0.5;
         }
-        //if Price more than 100 get 40% Discount Up to 40 LE
+        //if Price more than 100 
         public bool isBQualified(Order r)
         {
             return r.Price >= 100;
         }
+        //get 40% Discount Up to 40 LE
         public double B(Order r)
         {
             return (r.Price * 0.4) > 40 ? 40 : (r.Price * 0.4);
         }
-        //if Price more than 20 and less than 60 get 25% Discount Up to 30 LE
+        //if Price more than 20 and less than 60 
         public bool isCQualified(Order r)
         {
             return r.Price > 20 && r.Price < 60;
         }
+        //get 25% Discount Up to 30 LE
         public double C(Order r)
         {
             return (r.Price * 0.25) > 30 ? 30 : (r.Price * 0.25);
         }
-        //if Price more than 100 and Quantity more than 14 get 70% Discount
+        //if Price more than 100 and Quantity more than 14 
         public bool isDQualified(Order r)
         {
             return r.Price >= 100 && r.Quantity > 14;
         }
+        //get 70% Discount
         public double D(Order r)
         {
             return r.Price * 0.7;
